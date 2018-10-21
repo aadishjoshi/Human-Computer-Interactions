342fdb46d1fa6da9f6f656de9dd6c718adcf9648
aadishjoshi <emailaadish@gmail.com>
2018-10-20 20:02:39 -0500

    multithreading in c# final code

---
+++ asj170430Asg4 Multithreading with C#/asj170430Asg4/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace asj170430Asg4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.filePathBox = new System.Windows.Forms.TextBox();
+            this.searchBar = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Num = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Line = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.selectFile = new System.Windows.Forms.Button();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(246, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "File Path";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Search key";
+            // 
+            // filePathBox
+            // 
+            this.filePathBox.Location = new System.Drawing.Point(79, 40);
+            this.filePathBox.Name = "filePathBox";
+            this.filePathBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.filePathBox.Size = new System.Drawing.Size(383, 20);
+            this.filePathBox.TabIndex = 3;
+            // 
+            // searchBar
+            // 
+            this.searchBar.Location = new System.Drawing.Point(79, 75);
+            this.searchBar.Name = "searchBar";
+            this.searchBar.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.searchBar.Size = new System.Drawing.Size(383, 20);
+            this.searchBar.TabIndex = 4;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Num,
+            this.Line});
+            this.listView1.Location = new System.Drawing.Point(15, 137);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(528, 254);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Num
+            // 
+            this.Num.Text = "Number";
+            this.Num.Width = 54;
+            // 
+            // Line
+            // 
+            this.Line.Text = "Lines";
+            this.Line.Width = 468;
+            // 
+            // selectFile
+            // 
+            this.selectFile.Location = new System.Drawing.Point(468, 38);
+            this.selectFile.Name = "selectFile";
+            this.selectFile.Size = new System.Drawing.Size(75, 23);
+            this.selectFile.TabIndex = 6;
+            this.selectFile.Text = "Select File";
+            this.selectFile.UseVisualStyleBackColor = true;
+            this.selectFile.Click += new System.EventHandler(this.selectFile_Click);
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(468, 73);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchBtn.TabIndex = 7;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Location = new System.Drawing.Point(229, 9);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(0, 13);
+            this.messageLabel.TabIndex = 8;
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(232, 397);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 9;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(468, 73);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 10;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.ForeColor = System.Drawing.Color.MediumOrchid;
+            this.progressBar.Location = new System.Drawing.Point(15, 112);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(528, 10);
+            this.progressBar.Step = 1;
+            this.progressBar.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 427);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.selectFile);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.searchBar);
+            this.Controls.Add(this.filePathBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Search Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox filePathBox;
+        private System.Windows.Forms.TextBox searchBar;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button selectFile;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.ColumnHeader Num;
+        private System.Windows.Forms.ColumnHeader Line;
+    }
+}
+
---
+++ asj170430Asg4 Multithreading with C#/asj170430Asg4/Form1.cs
@@ -0,0 +1,282 @@
+﻿/*********************************************************************************************************************
+ * 
+ * MULTI-THREADED TEXT SEARCH PROGRAM
+ * ASSIGNMENT 04
+ * 
+ * The purpose of this program was to write a multithreaded program that searches a text file for a string. 
+ * Also it must find all occurrences of the string and show them in a list.
+ * 
+ * 
+ * Netid: asj170430
+ * First Name:  Aadish
+ * Middle Name: Santosh
+ * Last Name:   Joshi
+ * 
+ * Additional feature:
+ * Clear button is provided to wipe out all the search content.
+ * 
+ * Instructions:
+ * 1) Select the file using select file button. By default it is set to filter out any files other than text or .txt
+ * 2) Give a search query in query box. if remained empty, it will display empty message. else proceeds the search.
+ * 3) You can terminate the search inbetween.
+ * 4) Progressbar is provided to display search progress as per the line evaluation.
+ * 
+ * Written by Aadish Joshi (asj170430) at The University of Texas at Dallas
+ * Starting Aug'21, 2018
+**********************************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace asj170430Asg4
+{
+    public partial class Form1 : Form
+    {
+        // Global varibles are set so could be accessed from different functions as well. 
+        OpenFileDialog dialog = new OpenFileDialog();
+        private BackgroundWorker worker;
+        int totalLines;
+        double size;
+        int pbMaxiumum;
+        string filePath;
+        string searchQuery;
+        string labelMessage = "Select File to be evaluted";
+
+        /**************************************************************************
+         * Form1 is a main constructor
+        **************************************************************************/
+        public Form1()
+        {
+            InitializeComponent();
+            reload();
+        }
+
+        /**************************************************************************
+         * Reload function clears the data field and sets all values to defaults;
+        **************************************************************************/
+        public void reload()
+        {
+            totalLines = 0;
+            pbMaxiumum = 0;
+            filePath = "";
+            searchQuery = "";
+            messageLabel.Text = labelMessage;
+            searchBar.Enabled = false;
+            cancelBtn.Enabled = false;
+            cancelBtn.Visible = false;
+        }
+
+        /**************************************************************************
+         * BackgroundWorker constuctor. for multithreading purpose
+         * BackgroundWorker worker = new BackgroundWorker (true [for reporing process],true[for cancellation support])
+         * backgroundWorker methods are redeclaired such as dowork, runworkcompleted, progresschanged, and runworkerAsync();
+        **************************************************************************/
+        private void BGConstructor()
+        {
+            if(worker != null)
+            {
+                worker.Dispose();
+            }
+            worker = new BackgroundWorker {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = true
+            };
+
+            worker.DoWork += DoWork;
+            worker.RunWorkerCompleted += RunWorkerCompleted;
+            worker.ProgressChanged += ProgressChanged;
+            worker.RunWorkerAsync();
+
+        }
+
+        /**************************************************************************
+        * DoWork method of the backgroundworker class.
+        * searches the string result and if found match reports progress to Progresschanged.
+        * I am returning linenumber and string data through ReportProgress method of the worker
+        * 1000 ms delay is provided
+        **************************************************************************/
+        private void DoWork(object sender,DoWorkEventArgs e)
+        {
+            try
+            {
+                using(StreamReader reader = new StreamReader(filePath))
+                {
+                    string record = reader.ReadLine();
+                    while(record != null)
+                    {
+                        totalLines++;
+                        if (record.Contains(searchQuery))
+                        {
+                            System.Threading.Thread.Sleep(1000);
+                            worker.ReportProgress(totalLines, record);
+                        }
+                        record = reader.ReadLine();
+
+                        if(worker.CancellationPending == true)
+                        {
+                            e.Cancel = true;
+                            return;
+                        }
+                    }
+                   
+                }
+            }
+            catch (Exception e11)
+            {
+                MessageBox.Show("Error in searching!"+e11.ToString());
+            }
+        }
+
+        /**************************************************************************
+        * RunWorkerCompleted(sender, e)
+        * when the execution is completed or cancelled this method is triggered.
+        **************************************************************************/
+        private void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            searchBtn.Visible = false;
+            clearBtn.Enabled = true;
+            cancelBtn.Visible = false;
+            cancelBtn.Enabled = false;
+            if (e.Cancelled)
+            {
+                messageLabel.ForeColor = Color.Red;
+                messageLabel.Text = "Search cancelled!";
+
+            }
+            else
+            {
+                messageLabel.ForeColor = Color.Green;
+                progressBar.Value = pbMaxiumum;
+                if (listView1.Items.Count == 0)
+                {
+                    messageLabel.Text = "Match not found!";
+                }
+                else
+                {
+                    messageLabel.Text = "Search Completed!";
+                }
+                
+            }
+            
+        }
+        /**************************************************************************
+        * ProgressChanged(sender,e) catches worker.ReportProgress method call.
+        * Listview adds item.
+        * ProgressBar value grows with 1 step.
+        * ListView columns and headers are made autoadjustable
+        **************************************************************************/
+        private void ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            
+            ListViewItem result = new ListViewItem(new string[] { e.ProgressPercentage.ToString(), e.UserState.ToString() });
+            listView1.Items.Add(result);
+            listView1.View = View.Details;
+            if (progressBar.Value != progressBar.Maximum)
+            {
+                progressBar.PerformStep();
+            }
+            this.listView1.AutoResizeColumns(System.Windows.Forms.ColumnHeaderAutoResizeStyle.ColumnContent);
+            this.listView1.AutoResizeColumns(System.Windows.Forms.ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        /**************************************************************************
+        * SelectFile btn click class 
+        * filters all the files except txt files
+        * Progressbar size is allocated to maximum with the help of size of the file
+        **************************************************************************/
+        private void selectFile_Click(object sender, EventArgs e)
+        {
+            dialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = dialog.FileName;
+                size = new FileInfo(dialog.FileName).Length;
+                pbMaxiumum = (Convert.ToInt32(size) / 1000);
+                filePathBox.Text = filePath;
+                messageLabel.Text = "Path set correctly. Try Evaluating";
+                messageLabel.ForeColor = Color.Orange;
+            }
+            searchBar.Enabled = true;
+        }
+
+        /**************************************************************************
+        * ClearBtn is a clearbutton click handler
+        * reload function iscalled again to set it to default
+        **************************************************************************/
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            filePath = "";
+            filePathBox.Text = "";
+            searchBar.Text = "";
+            progressBar.Value = 0;
+            listView1.Clear();
+            cancelBtn.Visible = false;
+            cancelBtn.Enabled = false;
+            searchBar.Visible = true;
+            searchBtn.Visible = true;
+            messageLabel.Text = labelMessage;
+            messageLabel.ForeColor = Color.Black;
+            reload();
+        }
+
+        /**************************************************************************
+        * searchBtn is a searchBtn click handler
+        * Validates if the search query is null if so, displays error message
+        * else BackgroundWorker constructor is called
+        **************************************************************************/
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            searchQuery = searchBar.Text;
+            if(searchQuery == "")
+            {
+                messageLabel.Text = "Please give a valid search query";
+                messageLabel.ForeColor = Color.Red;
+            }
+            else
+            {
+                searchBar.Visible = false;
+                cancelBtn.Visible = true;
+                cancelBtn.Enabled = true;
+                clearBtn.Enabled = false;
+                messageLabel.Text = "Searching...";
+                messageLabel.ForeColor = Color.BlueViolet;
+                progressBar.Maximum = pbMaxiumum;
+                progressBar.Value = 0;
+                progressBar.Step = 1;
+                BGConstructor();
+
+            }
+            
+
+        }
+
+        /**************************************************************************
+        * cancelBtn is a cancelButton click handler
+        * worker.CancelAsync() trigger the cancels event handle
+        * The current backgroundworker is disposed.
+        **************************************************************************/
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            searchBar.Visible = true;
+            cancelBtn.Visible = false;
+            clearBtn.Enabled = true;
+            worker.CancelAsync();
+            worker.Dispose();
+            messageLabel.Text = "Cancelled!";
+            messageLabel.ForeColor = Color.Red;
+
+        }
+
+       
+
+    }
+}
